b619dcbec78352af06716df4681754684d17629f
Franco Buccellato <franco_buccellato@carrefour.com>
2026-04-08 14:50:34 -0300

    Agrego orden a schema

--- modelos/recomendaciones.js
+++ modelos/recomendaciones.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-const recomendacionSchema = new mongoose.Schema({
+const RecomendacionSchema = new mongoose.Schema({
     id: { type: String, required: true },
-    descripcion: { type: String, required: true }
+    descripcion: { type: String, required: true },
+    orden: { type: Number, default: 0 } // <--- SI NO ESTÁ ESTO, MONGO NO LO GUARDA
 });
 
-module.exports = mongoose.model('Recomendaciones', recomendacionSchema);
+module.exports = mongoose.model('Recomendacion', RecomendacionSchema);
